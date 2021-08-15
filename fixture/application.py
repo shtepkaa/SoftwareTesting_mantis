a6e1e68ee6bc50824f2fbdbc25ccffe417b70b9c
shtepkaa <61048714+shtepkaa@users.noreply.github.com>
2021-08-15 17:57:25 +0300

    Получение почты по протоколу POP3

--- fixture/application.py
+++ fixture/application.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.project import ProjectHelper
 from fixture.james import JamesHelper
+from fixture.signup import SignupHelper
+from fixture.mail import MailHelper
 
 
 class Application:
@@ -16,10 +18,12 @@ class Application:
         else:
             raise ValueError("Unrecognized browser %s" % browser)
         self.session = SessionHelper(self)
-        self.config = config
-        self.base_url = config['web']["baseurl"]
         self.project = ProjectHelper(self)
         self.james = JamesHelper(self)
+        self.signup = SignupHelper(self)
+        self.mail = MailHelper(self)
+        self.config = config
+        self.base_url = config['web']["baseurl"]
 
     def is_valid(self):
         try:
--- test/test_signup_new_account.py
+++ test/test_signup_new_account.py
@@ -1,8 +1,18 @@
+import random
+import string
+
+
+def random_username(prefix, maxlen):
+    symbols = string.ascii_letters
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
 def test_signup_new_account(app):
-    username = "user1"
-    # username = app.generator.random_name_of_project("start", 7)
+    username = random_username("user_", 10)
     password = "test"
-    # email = username + "@localhost"
+    email = username + "@localhost"
     app.james.ensure_user_exists(username, password)
-    # app.signup.new_user(username, email, password)
-    # assert app.soap.can_login(username, password)
+    app.signup.new_user(username, email, password)
+    app.session.login(username, password)
+    assert app.session.is_logged_in_as(username)
+    app.session.logout()
